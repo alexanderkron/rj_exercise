bd8a3844f510f9dcc568b338bedff1d87386605c
Ryan Kuczka <ryan.kuczka@gmail.com>
2019-02-12 21:18:41 -0500

    Initial commit -- Rejoiner Exercise

---
+++ tracker/models.py
@@ -0,0 +1,16 @@
+import uuid
+
+from django.db import models
+
+
+class Cart(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+
+
+class Item(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    cart = models.ForeignKey(Cart, related_name='items', on_delete=models.CASCADE)
+
+    product_id = models.TextField()
+    name = models.TextField()
+    price = models.IntegerField()
